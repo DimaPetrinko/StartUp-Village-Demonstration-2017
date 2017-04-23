1dadf2bb7d35854a90fbd54530e981dfe3af26c1
Blind56 <petrinko6@gmail.com>
2017-04-23 05:51:33 +0300

    VR setup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    const int MAIN_MENU_SCENE_INDEX = 1;
+
+    private void Start()
+    {
+        LoadScene(MAIN_MENU_SCENE_INDEX);
+    }
+
+    public bool IsInMenu()
+    {
+        return SceneManager.sceneCount < 2 || (SceneManager.GetActiveScene() == SceneManager.GetSceneAt(MAIN_MENU_SCENE_INDEX));
+    }
+
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+
+    private SceneController sceneController;
+
+    private void OnLevelWasLoaded(int level)
+    {
+        sceneController = FindObjectOfType<SceneController>();
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        transform.position = sceneController.spawnSpot.transform.position;
+        if (!sceneController.gameManager.IsInMenu())
+        {
+            rigidbody.isKinematic = true;
+        }
+        else
+        {
+            rigidbody.isKinematic = false;
+        }
+    }
+
+}
