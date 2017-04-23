70dea1bee7628cac4690f83d39bbdc4c283c80fa
Blind56 <petrinko6@gmail.com>
2017-04-23 21:51:18 +0300

    docmmt

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,67 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody rigidbody;
+    public Rigidbody myRigidbody;
+    public float acceleration = 1f;
+    public float maxWalkSpeed = 5f;
 
+    private GameObject mainCamera;
     private SceneController sceneController;
+    private bool move = false;
+    private bool isInMenu = true;
+
+    private void Start()
+    {
+        mainCamera = Camera.main.gameObject;
+    }
+
+    private void Update()
+    {
+        if (!isInMenu && GvrViewer.Instance.Triggered)
+        {
+            move = !move;
+            Debug.Log(move);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
 
     private void OnLevelWasLoaded(int level)
     {
         sceneController = FindObjectOfType<SceneController>();
+        isInMenu = sceneController.gameManager.IsInMenu();
         Spawn();
     }
 
     private void Spawn()
     {
+        move = false;
         transform.position = sceneController.spawnSpot.transform.position;
-        if (!sceneController.gameManager.IsInMenu())
+        myRigidbody.velocity = Vector3.zero;
+        myRigidbody.isKinematic = isInMenu;
+    }
+
+    private void Move()
+    {
+        if (move)
         {
-            rigidbody.isKinematic = true;
+            Vector3 direction = new Vector3(mainCamera.transform.forward.x, 0, mainCamera.transform.forward.z).normalized;
+            Debug.Log(myRigidbody.velocity.magnitude);
+            if (myRigidbody.velocity.magnitude < maxWalkSpeed)
+            {
+                myRigidbody.AddForce(direction * acceleration, ForceMode.Force);
+            }
         }
-        else
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Door"))
         {
-            rigidbody.isKinematic = false;
+            sceneController.ReturnToMenu();
         }
     }
 
