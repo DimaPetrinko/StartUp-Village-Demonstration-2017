25e4edc18465b10fb9ba5feb4fc8a74f250da263
Blind56 <petrinko6@gmail.com>
2017-04-24 22:43:23 +0300

    fixes and stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -43,6 +43,7 @@ public class PlayerController : MonoBehaviour
     {
         move = false;
         transform.position = sceneController.spawnSpot.transform.position;
+        transform.rotation = sceneController.spawnSpot.transform.rotation;
         myRigidbody.velocity = Vector3.zero;
         myRigidbody.isKinematic = isInMenu;
     }
